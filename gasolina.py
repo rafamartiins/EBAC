6a3b2563ecc509533da07a66a6749490ae2a6e68
rafamartiins <rafa.s.martinss@gmail.com>
2025-02-07 19:26:18 +0000

    Adicionando arquivos gerados

--- gasolina.py
+++ gasolina.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('gasolina.csv')
-df.head()
 
-# Criar o gráfico
+
 plt.figure(figsize=(10, 5))
 sns.lineplot(data=df, x='dia', y='venda', marker='o', color='b')
 
@@ -15,6 +14,7 @@ plt.title('Variação do Preço da Gasolina ao Longo dos Dias')
 plt.xlabel('Dia')
 plt.ylabel('Preço (R$)')
 plt.grid(True)
+
 
 plt.savefig('gasolina.png')
 plt.show()
